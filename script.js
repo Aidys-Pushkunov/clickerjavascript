af82e49b0592d22997e98840769ac4bdafb446f3
Aidys-Pushkunov <62947297+Aidys-Pushkunov@users.noreply.github.com>
2021-01-04 01:07:09 +0700

    )

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-'use strict'
-
-let buttonId = document.getElementById('button');
+let buttonId = document.getElementById('buttonId');
 let h1       = document.getElementById('h1');
 let count    = 0;
-
 
 buttonId.addEventListener('click', function() {
 
 	if (count == 0) {
-		setTimeout(gameOver, 10000);
+		setTimeout(secTime, 15000);
+	};
+
+	if (count == 100) {
+		h1.textContent = 'You The Best!';
 	};
 
 	count++;
-
-	if(count == 10) h1.textContent = 'Exelent!';
-	else if(count == 50) h1.textContent = 'Good';
-	else if(count == 100) {
-		h1.textContent = 'You win'
-		alert('You win!');
-	}
-	else h1.textContent = 'Count: ' + count;
+	h1.textContent = 'Count: ' + count;
 });
 
-function gameOver() {
+function secTime() {
 	alert('Game Over!');
-	alert('Your Count: ' + count);
+	alert('Your count: ' + count);
 	count = 0;
 	h1.textContent = 'Count: ' + count;
-};
+}
 
